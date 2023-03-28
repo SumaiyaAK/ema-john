4298d939998a0279faea11014f3b78ccfc5df644
SumaiyaAK <sumaiyasrn6@gmail.com>
2023-03-28 23:10:23 +0600

    retrieve product from stored cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -14,10 +14,26 @@ const Shop = () => {
         .then(data =>setProducts(data))
     },[])
 
-    useEffect(()=>{
-        const storeCart = getShoppingCart();
-        console.log(storeCart)
-    },[])
+    useEffect(() =>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id of the addedProduct
+        for(const id in storedCart){
+            // get product from products state by using id
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4 : add the added product to the saved cart
+                savedCart.push(addedProduct);
+            }
+            console.log('added product', addedProduct)
+
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    },[products])
 
     const handleAddToCart = (product) =>{
         const newCart = [...cart, product];
